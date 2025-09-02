bb7ad3236465b8ddbd0a73d2e131c498034c3edc
MichielvanHattem <Michielvanhattem1968@gmail.com>
2025-09-02 23:02:44 +0200

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,7 @@
+// middleware/authMiddleware.js
 export default (req,res,next) => {
-  const token = req.cookies.auth_token;
-  if(!token) return res.status(401).json({ error:'Geen token' });
+  const token = req.cookies?.auth_token;
+  if (!token) return res.status(401).json({ error:'Geen token' });
   req.token = token;
   next();
 };
