35970a1d232eba7ca5abfbd50a9a073925a8a569
MichielvanHattem <Michielvanhattem1968@gmail.com>
2025-09-02 23:00:35 +0200

    Update sharepoint.js

--- utils/sharepoint.js
+++ utils/sharepoint.js
@@ -1,7 +1,12 @@
+// utils/sharepoint.js
 import axios from 'axios';
+
+// GRAPH_API_URL: bv. https://graph.microsoft.com/v1.0/me/drive/root/children
 export async function getSharePointFiles(token){
-  const { data } = await axios.get(process.env.GRAPH_API_URL,{
-    headers:{ Authorization:`Bearer ${token}` }
+  const url = process.env.GRAPH_API_URL;
+  if (!url) throw new Error('GRAPH_API_URL ontbreekt');
+  const { data } = await axios.get(url, {
+    headers: { Authorization: `Bearer ${token}` }
   });
   return data;
 }
